71eb2bce04b3e38095ffcc0536e33dc32882b226
whroman <me@walterroman.com>
2015-02-28 16:48:54 -0700

    setting palette for project. layout work

--- server/uwrtourist/routes.py
+++ server/uwrtourist/routes.py
@@ -12,7 +12,7 @@ def add_pages():
 
 @app.route("/")
 def homepage():
-    title = "Underwater Rugby Tourist"
+    title = False
     return render_template("pages/home.jade", title=title)
 
 @app.route("/about")
