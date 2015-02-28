d8328c53c309b091d966d7ebf933cc241b207c3b
whroman <me@walterroman.com>
2015-02-28 10:54:47 -0700

    moving gulp to client dir

--- client/Gulpfile.js
+++ client/Gulpfile.js
@@ -1,22 +1,24 @@
 var gulp = require('gulp');
-var tasks = require('./client/tasks');
+var tasks = require('./tasks');
 var spawn = require('child_process').spawn;
 
 var paths = {};
-
+paths.projectRoot = '../';
 paths.resources = './client/resources/';
-
 paths.build = paths.resources + 'build/';
-
-paths.scss = {};
+paths.scss = {};function logStd (data) {
+    var message = data.toString();
+    if (data) console.log(message);
+}
 paths.scss.root = paths.resources + 'scss/';
 paths.scss.src = [paths.scss.root + 'init.scss'];
 paths.scss.watch = [paths.scss.root + '**/*.scss'];
-
 paths.js = {};
 paths.js.root = paths.resources + 'js/';
 paths.js.build = 'build.js';
 paths.js.watch = [paths.resources + 'js/**/*.js'];
+
+process.chdir(paths.projectRoot);
 
 gulp.task('js:lint', function() {
     var options = {
@@ -38,15 +40,14 @@ gulp.task('scss:compile', function() {
 
 gulp.task('server', function() {
     var child = spawn('bin/runserver');
-    child.stdin.on('data', logStd);
     child.stdout.on('data', logStd);
     child.stderr.on('data', logStd);
-});
 
-function logStd (data) {
-    var message = data.toString();
-    if (data) console.log(message);
-}
+    function logStd (data) {
+        var message = data.toString();
+        if (data) console.log(message);
+    }
+});
 
 gulp.task('watch', function() {
     gulp.watch(paths.js.watch, ['js:lint']);
