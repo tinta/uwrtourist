a3cf2e951ca16abaf33c62f9012b3dceafc66203
Cristina Munoz <hi@xmunoz.com>
2015-04-03 17:05:40 -0600

    Add all teams
    Bunch o' fixes to add unicode support for crazily named german teams.

--- server/manage.py
+++ server/manage.py
@@ -2,15 +2,22 @@ from uwrtourist.models import db
 from uwrtourist.routes import app
 import uwrtourist.models as um
 
+# team data
+from uwrtourist.fixtures.teams import seed
+from uwrtourist.fixtures.geo_data import all_teams
+
+# other libraries
+import requests
+
 def init_db():
     # do this import so that all models are create when create_all is called
     db.create_all()
 
 def populate_db():
-    from uwrtourist.fixtures.teams import seed
     # iterate over fixtures and add to db
     for team in seed:
-        new_team = um.Team(team.get("name"), team.get("city"))
+        new_team = um.Team(team.get("name"), team.get("city"),
+                           team.get("country"))
 
         for l in team["locations"]:
             new_team.practice_locations.append(
@@ -43,6 +50,37 @@ def populate_db():
         db.session.add(new_team)
     db.session.commit()
 
+    # Now add geodata
+    base_url = "http://maps.googleapis.com/maps/api/geocode/json"
+    for team in all_teams:
+        team_name = team[0].decode("utf-8")
+        coordinates = ",".join(str(v) for v in team[1:3])
+        team_links = team[3:]
+        response = requests.get(base_url, params={"latlng": coordinates, "sensor": False})
+        # simply grab first result
+        if response.json()["status"] == "OK":
+            geodata = response.json()["results"][0]["address_components"]
+            country = ""
+            city = ""
+            state = ""
+            for data in geodata:
+                if "country" in data["types"]:
+                    country = data["short_name"]
+                elif "locality" in data["types"]:
+                    city = data["short_name"]
+                elif "administrative_area_level_1" in data["types"]:
+                    state = data["short_name"]
+
+            location = ", ".join([city, state])
+            new_team = um.Team(team_name, location, country)
+            for l in team_links:
+                if "uwr1.de" not in l:
+                    new_team.links.append(um.Link(l.decode("utf-8")))
+            db.session.add(new_team)
+        else:
+            print "error: ", team
+
+    db.session.commit()
 
 def main():
     with app.app_context():
