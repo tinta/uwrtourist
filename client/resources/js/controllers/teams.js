3e5a34b7b8574f98fafcdd2fa2dc4e0ec868ffb4
whroman <me@walterroman.com>
2015-04-03 12:34:42 -0600

    pulling in teams from db. table modications to filtering and ordering

--- client/resources/js/controllers/teams.js
+++ client/resources/js/controllers/teams.js
@@ -15,13 +15,7 @@ angular.module('ControllerTeams', [
 ){
     $scope.table = new Table(window.teams);
 
-    $scope.table.order.set('Name');
-
-    $scope.table.filter.apply = function (obj) {
-        var val = $scope.table.filter.val;
-        var re = new RegExp(val, 'i');
-        return !val || re.test(obj['Name']) || re.test(obj['Region']);
-    };
+    $scope.table.keys.order.set('name');
 
     $scope.typeOf = function(input) {
         return typeof input;
--- client/resources/js/table.js
+++ client/resources/js/table.js
@@ -9,44 +9,53 @@ angular
     var Table = (function() {
         var Table = function (collection) {
             this.all = collection;
-            // Used for filtering
-            this.filter = {
-                val: ''
-            };
-            // `queried` is filled when `all` is filtered
-            this.queried = [];
-            // List of keys that are properties for each object in `schools`
-            this.keys = [];
-            this.setKeys(collection);
-            console.log(this.keys[0]);
 
-            this.order = {
-                value   :  this.keys[0],
+            // List of keys that are properties for each object in `schools`
+            this.keys.set(collection);
+        };
+
+        Table.prototype.filter = (function(){
+            var _filter = {};
+            _filter.value = '';
+            _filter.apply = function (obj) {
+                console.log(obj, _filter)
+                var re = new RegExp(_filter.value, 'i');
+                return !_filter.value || re.test(obj['name']) || re.test(obj['location']);
+            }
+
+            return _filter;
+        })();
+
+        Table.prototype.keys = {
+            list: [],
+            set: function (arr) {
+                var i = 0;
+                var arrLen = arr.length;
+                var key;
+                for (i; i < arrLen; i++) {
+                    for (key in arr[i]) {
+                        if (this.list.indexOf(key) === -1) {
+                            this.list.push(key);
+                        }
+                    }
+                }
+
+                console.log(this)
+                this.order.set(this.list[0]);
+            },
+            order: {
+                value   :  null,
                 latestInput : null,
                 reverse : false,
-                set     : function(key) {
+                set     : function (key) {
                     this.latestInput = key;
                     doubleQuotedKey = "'" + key + "'";
                     if (this.value === doubleQuotedKey) {
-                        this.reverse = !this.reverse; 
+                        this.reverse = !this.reverse;
                     } else {
                         this.value = doubleQuotedKey;
                     }
                     return this;
-                }
-            };
-
-        };
-
-        Table.prototype.setKeys = function(arr) {
-            var i = 0;
-            var arrLen = arr.length;
-            var key;
-            for (i; i < arrLen; i++) {
-                for (key in arr[i]) {
-                    if (this.keys.indexOf(key) === -1) {
-                        this.keys.push(key);
-                    }
                 }
             }
         };
