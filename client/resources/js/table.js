4403c65f84c78ee562b88c6b525484a53ef5d329
whroman <me@walterroman.com>
2015-04-03 16:58:52 -0600

    table pagination!

--- client/resources/js/table.js
+++ client/resources/js/table.js
@@ -9,18 +9,24 @@ angular
     var Table = (function() {
         var Table = function (collection, whiteList) {
             this.rows = collection;
+            this.queried = [];
             this.keys = whiteList;
 
             this.order = order.bind(this)();
             this.filter = filter.bind(this)();
+            this.paginate = paginate.bind(this)();
+
+            this.filter.apply();
         };
 
         function filter () {
+            var root = this;
+
             var _filter = {};
             _filter.value = '';
-            _filter.apply = function (obj) {
-                var re = new RegExp(_filter.value, 'i');
-                return !_filter.value || re.test(obj['name']) || re.test(obj['location']);
+            _filter.apply = function () {
+                root.queried = $filter('filter')(root.rows, _filter.value);
+                root.paginate.update();
             };
 
             return _filter;
@@ -43,6 +49,63 @@ angular
             };
 
             return _order;
+        }
+
+        function paginate () {
+            var root = this;
+
+            var _paginate = {};
+
+            _paginate.disabled = true;
+
+            _paginate.pages = {};
+            _paginate.pages.current = 0;
+            _paginate.pages.maxRows = 20;
+            _paginate.pages.length = 0;
+
+            // Total number of pages for current set of queried data
+            determineNumOfPages = function() {
+                var pages = Math.ceil(root.queried.length/this.pages.maxRows);
+                return pages;
+            };
+
+            // `disableButtons` returns `true` if view's `previous` and `next` buttons should be disabled
+            shouldNavBeDisabled = function() {
+                var shouldBeDisabled = this.pages.length <= 1;
+                return shouldBeDisabled;
+            };
+
+            _paginate.update = function () {
+                this.pages.length = determineNumOfPages.bind(this)();
+                this.disabled = shouldNavBeDisabled.bind(this)();
+            };
+
+            _paginate.displayCurrent = function() {
+                if (this.pages.length === 0) return 0;
+                var page = this.pages.current + 1;
+                if (page <= 0) page = this.pages.length + page;
+                return page;
+            };
+
+            // Go back one page
+            _paginate.previous = function () {
+                if (this.pages.current > 0) {
+                    this.pages.current -= 1;
+                } else {
+                    this.pages.current = this.pages.length - 1;
+                }
+            };
+
+            // Go forward one page
+            _paginate.next = function () {
+                if (this.pages.current < this.pages.length - 1) {
+                    this.pages.current += 1;
+                } else {
+                    this.pages.current = 0;
+                }
+            };
+
+            return _paginate;
         }
 
         return Table;
