52740f16dd21ba91f75a5aac1d0be847d32470d3
whroman <me@walterroman.com>
2016-01-15 02:43:08 +0100

    Updating add team page design.

--- client/resources/js/models/AddTeamFormModel.js
+++ client/resources/js/models/AddTeamFormModel.js
@@ -8,42 +8,43 @@ angular
 // Dependency Injections
     $rootScope
 ){
-    var dynamicFields = (function () {
-        function dynamicFields (defaults) {
-            var dyFields = {};
-            dyFields.all = [];
-            dyFields.create = function (cb) {
-                var newField = angular.copy(defaults);
-                this.all.push(newField);
-                if (cb) cb(newField);
-                return newField;
-            };
-            dyFields.remove = function (index) {
-                this.all.splice(index, 1);
-            };
+    var DynamicFields = (function () {
+        var DynamicFields = function (defaults) {
+            this.all = [];
+            this.defaults = defaults;
+        };
 
-            return dyFields;
-        }
-        return dynamicFields;
+        DynamicFields.prototype.create = function (cb) {
+            var newField = angular.copy(this.defaults);
+            this.all.push(newField);
+            if (cb) cb(newField);
+            return newField;
+        };
+
+        DynamicFields.prototype.remove = function (index) {
+            this.all.splice(index, 1);
+        };
+
+        return DynamicFields;
     })();
 
     var FormClass = (function() {
         var Form = function () {
-            this.links = new dynamicFields({val: ''});
+            this.links = new DynamicFields({val: ''});
 
-            this.contacts = new dynamicFields({
+            this.contacts = new DynamicFields({
                 name: '',
                 email: '',
                 title: ''
             });
 
-            this.practiceLocations = new dynamicFields({
+            this.practiceLocations = new DynamicFields({
                 name: '',
                 city: '',
                 region: '',
                 country: '',
                 postalCode: '',
-                times: new dynamicFields({
+                times: new DynamicFields({
                     startTime: '',
                     endTime: '',
                     day: '',
@@ -68,7 +69,7 @@ angular
 
         Form.prototype.newPractice = function () {
             this.practiceLocations.create(function(newlyCreatedField) {
-                newlyCreatedField.times.create();
+                // newlyCreatedField.times.create();
             });
         };
 
